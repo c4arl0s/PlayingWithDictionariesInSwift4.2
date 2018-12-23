7f63539c09249b7f98eb528139ac7057bb6df527
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-23 16:34:09 -0600

    Playing with dictionaries to understand jsonObjects

---
+++ PlayingWithDictionariesInSwift4.2/PlayingWithDictionariesInSwift4.2/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PlayingWithDictionariesInSwift4.2
+//
+//  Created by Carlos Santiago Cruz on 12/23/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Creating an Empty Dictionary
+    var namesOfIntegers = [Int: String]()
+    // Creating a Dictionary with a Dictionary Literal
+    var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+    var nomenclature: [String: String] = ["60HP": "High pressure"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // namesOfIntegers now contains 1 key-value pair
+        namesOfIntegers[16] = "sixteen"
+        // namesOfIntegers is once again an empty dictionary of type [Int: String]
+        print(namesOfIntegers)
+        // append another element
+        namesOfIntegers[10] = "ten"
+        print(namesOfIntegers)
+        namesOfIntegers[11] = "eleven"
+        print(namesOfIntegers)
+        airports["MX"] = "Mexico City"
+        print(airports)
+        
+        for (airportCode, airportName) in airports {
+            print("\(airportCode): \(airportName)")
+        }
+        for airportCode in airports.keys {
+            print("Airport code: \(airportCode)")
+        }
+        for airportName in airports.values {
+            print("Airport name: \(airportName)")
+        }
+        let airportCodes = [String](airports.keys)
+        print(airportCodes)
+        let airportNames = [String](airports.values)
+        print(airportNames)
+        
+    }
+
+
+}
+
